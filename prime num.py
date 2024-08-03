6ecd24655db7de85257c892de748248114736298
Yasas03 <yasas03.official@gmail.com>
2024-08-03 12:55:40 +0530

    Prime Numbers.py

--- prime num.py
+++ prime num.py
@@ -1,7 +1,7 @@
 n = int(input(f'Enter the number : '))
 numbers = list(range(2,n+1))
 m=2
-while m*m <n: 
+while m <= n: 
         if m in numbers:
             for num in numbers[:]:
                 if num % m == 0 and num != m:
